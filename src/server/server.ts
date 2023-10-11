00d0e1864a0878806a6914e1d51e1428bf870fa2
rajahwu <vmradford@gmail.com>
2023-10-11 12:55:03 -0400

    add react

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import os from "node:os";
 
-import config from "./config"; "./config";
+import config from "./config";
 
 const server = express();
 
@@ -10,12 +10,10 @@ server.use(express.static("public"));
 server.set("view engine", "ejs");
 
 server.use("/", (req, res) => {
-  res.render("index", {
-    content: "EJS is <em>cool</em>",
-  });
+  res.render("index");
 });
 
-server.listen(config.PORT, () => {
+server.listen(Number(config.PORT), config.HOST, () => {
   console.info(
     `Express server is running at ${config.SERVER_URL} 🚀`,
     `Free Men ${os.freemem() / 1024 / 1024}`
